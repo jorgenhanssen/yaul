f001997059391beb3636afd4efb34364f77827b6
Jørgen Hanssen <jorkiz97@gmail.com>
2024-04-03 19:18:37 +0200

    Renamed argx => ax

--- src/syscall/aarch64.rs
+++ src/syscall/aarch64.rs
@@ -11,77 +11,70 @@ pub unsafe fn syscall0(n: usize) -> usize {
     ret
 }
 
-pub unsafe fn syscall1(n: usize, arg1: usize) -> usize {
+pub unsafe fn syscall1(n: usize, a1: usize) -> usize {
     let mut ret: usize;
     asm!(
         "svc 0",
         in("x8") n,
-        in("x0") arg1,
+        in("x0") a1,
         lateout("x0") ret,
         options(nostack, preserve_flags)
     );
     ret
 }
 
-pub unsafe fn syscall2(n: usize, arg1: usize, arg2: usize) -> usize {
+pub unsafe fn syscall2(n: usize, a1: usize, a2: usize) -> usize {
     let mut ret: usize;
     asm!(
         "svc 0",
         in("x8") n,
-        in("x0") arg1,
-        in("x1") arg2,
+        in("x0") a1,
+        in("x1") a2,
         lateout("x0") ret,
         options(nostack, preserve_flags)
     );
     ret
 }
 
-pub unsafe fn syscall3(n: usize, arg1: usize, arg2: usize, arg3: usize) -> usize {
+pub unsafe fn syscall3(n: usize, a1: usize, a2: usize, a3: usize) -> usize {
     let mut ret: usize;
     asm!(
         "svc 0",
         in("x8") n,
-        in("x0") arg1,
-        in("x1") arg2,
-        in("x2") arg3,
+        in("x0") a1,
+        in("x1") a2,
+        in("x2") a3,
         lateout("x0") ret,
         options(nostack, preserve_flags)
     );
     ret
 }
 
-pub unsafe fn syscall4(n: usize, arg1: usize, arg2: usize, arg3: usize, arg4: usize) -> usize {
+pub unsafe fn syscall4(n: usize, a1: usize, a2: usize, a3: usize, a4: usize) -> usize {
     let mut ret: usize;
     asm!(
         "svc 0",
         in("x8") n,
-        in("x0") arg1,
-        in("x1") arg2,
-        in("x2") arg3,
-        in("x3") arg4,
+        in("x0") a1,
+        in("x1") a2,
+        in("x2") a3,
+        in("x3") a4,
         lateout("x0") ret,
         options(nostack, preserve_flags)
     );
     ret
 }
 
-pub unsafe fn syscall5(
-    n: usize,
-    arg1: usize,
-    arg2: usize,
-    arg3: usize,
-    arg4: usize,
-    arg5: usize,
-) -> usize {
+pub unsafe fn syscall5(n: usize, a1: usize, a2: usize, a3: usize, a4: usize, a5: usize) -> usize {
     let mut ret: usize;
     asm!(
         "svc 0",
         in("x8") n,
-        in("x0") arg1,
-        in("x1") arg2,
-        in("x2") arg3,
-        in("x3") arg4,
-        in("x4") arg5,
+        in("x0") a1,
+        in("x1") a2,
+        in("x2") a3,
+        in("x3") a4,
+        in("x4") a5,
         lateout("x0") ret,
         options(nostack, preserve_flags)
     );
@@ -90,23 +83,23 @@ pub unsafe fn syscall5(
 
 pub unsafe fn syscall6(
     n: usize,
-    arg1: usize,
-    arg2: usize,
-    arg3: usize,
-    arg4: usize,
-    arg5: usize,
-    arg6: usize,
+    a1: usize,
+    a2: usize,
+    a3: usize,
+    a4: usize,
+    a5: usize,
+    a6: usize,
 ) -> usize {
     let mut ret: usize;
     asm!(
         "svc 0",
         in("x8") n,
-        in("x0") arg1,
-        in("x1") arg2,
-        in("x2") arg3,
-        in("x3") arg4,
-        in("x4") arg5,
-        in("x5") arg6,
+        in("x0") a1,
+        in("x1") a2,
+        in("x2") a3,
+        in("x3") a4,
+        in("x4") a5,
+        in("x5") a6,
         lateout("x0") ret,
         options(nostack, preserve_flags)
     );
