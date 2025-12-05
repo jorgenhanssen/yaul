0e7279a6d8c28263736551501b1350e0f654f847
Jørgen Hanssen <jorkiz97@gmail.com>
2025-12-05 16:22:15 +0100

    feat: aarch64 syscall support (#6)
    * feat: macos aarch64 syscall support
    
    * Don't let aarch64 be macos specific
    
    * Comment fix
    
    * Renamed argx => ax
    
    * made it work on an aarch64 mac
    
    * update docs

---
+++ src/syscall/aarch64.rs
@@ -0,0 +1,111 @@
+use core::arch::asm;
+
+// macOS syscalls have an offset of 0x2000000 (SYSCALL_CLASS_UNIX << SYSCALL_CLASS_SHIFT)
+// https://github.com/apple-oss-distributions/xnu/blob/main/osfmk/mach/syscall_sw.h
+const SYSCALL_SHIFT: usize = 0x2000000;
+
+pub unsafe fn syscall0(n: usize) -> usize {
+    let mut ret: usize;
+    asm!(
+        "svc 0x80",
+        in("x16") n + SYSCALL_SHIFT,
+        lateout("x0") ret,
+        options(nostack, preserves_flags)
+    );
+    ret
+}
+
+pub unsafe fn syscall1(n: usize, a1: usize) -> usize {
+    let mut ret: usize;
+    asm!(
+        "svc 0x80",
+        in("x16") n + SYSCALL_SHIFT,
+        in("x0") a1,
+        lateout("x0") ret,
+        options(nostack, preserves_flags)
+    );
+    ret
+}
+
+pub unsafe fn syscall2(n: usize, a1: usize, a2: usize) -> usize {
+    let mut ret: usize;
+    asm!(
+        "svc 0x80",
+        in("x16") n + SYSCALL_SHIFT,
+        in("x0") a1,
+        in("x1") a2,
+        lateout("x0") ret,
+        options(nostack, preserves_flags)
+    );
+    ret
+}
+
+pub unsafe fn syscall3(n: usize, a1: usize, a2: usize, a3: usize) -> usize {
+    let mut ret: usize;
+    asm!(
+        "svc 0x80",
+        in("x16") n + SYSCALL_SHIFT,
+        in("x0") a1,
+        in("x1") a2,
+        in("x2") a3,
+        lateout("x0") ret,
+        options(nostack, preserves_flags)
+    );
+    ret
+}
+
+pub unsafe fn syscall4(n: usize, a1: usize, a2: usize, a3: usize, a4: usize) -> usize {
+    let mut ret: usize;
+    asm!(
+        "svc 0x80",
+        in("x16") n + SYSCALL_SHIFT,
+        in("x0") a1,
+        in("x1") a2,
+        in("x2") a3,
+        in("x3") a4,
+        lateout("x0") ret,
+        options(nostack, preserves_flags)
+    );
+    ret
+}
+
+pub unsafe fn syscall5(n: usize, a1: usize, a2: usize, a3: usize, a4: usize, a5: usize) -> usize {
+    let mut ret: usize;
+    asm!(
+        "svc 0x80",
+        in("x16") n + SYSCALL_SHIFT,
+        in("x0") a1,
+        in("x1") a2,
+        in("x2") a3,
+        in("x3") a4,
+        in("x4") a5,
+        lateout("x0") ret,
+        options(nostack, preserves_flags)
+    );
+    ret
+}
+
+pub unsafe fn syscall6(
+    n: usize,
+    a1: usize,
+    a2: usize,
+    a3: usize,
+    a4: usize,
+    a5: usize,
+    a6: usize,
+) -> usize {
+    let mut ret: usize;
+    asm!(
+        "svc 0x80",
+        in("x16") n + SYSCALL_SHIFT,
+        in("x0") a1,
+        in("x1") a2,
+        in("x2") a3,
+        in("x3") a4,
+        in("x4") a5,
+        in("x5") a6,
+        lateout("x0") ret,
+        options(nostack, preserves_flags)
+    );
+    ret
+}
