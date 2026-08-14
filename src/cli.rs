5646201df8b778fd4fe80d39b6e79488c3d1cf00
Jørgen Hanssen <jorkiz97@gmail.com>
2026-08-14 16:47:35 +0200

    update Cargo.toml

--- src/cli.rs
+++ src/cli.rs
@@ -2,10 +2,10 @@ use clap::Parser;
 use std::path::PathBuf;
 
 #[derive(Parser)]
-#[command(name = "YAUL")]
+#[command(name = "yaul")]
 #[command(author = "Jørgen Hanssen")]
-#[command(version = "0.1")]
-#[command(about = "Runtime for the YAUL language", long_about = None)]
+#[command(version = "0.1.0")]
+#[command(about = "Yet Another Useless Language")]
 pub struct CLI {
     #[arg(long)]
     pub logs: Option<String>,
